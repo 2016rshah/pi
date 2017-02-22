ea607cbf7bea4d8eb6e0d08a46326b2a409527c0
Clifford Crouch <flamefire333@gmail.com>
2017-02-22 17:16:30 -0600

    Fixed graphics stuff

--- p5.c
+++ p5.c
@@ -196,6 +196,8 @@ static int struct_decode_type_np = 0;
 
 static struct user_operator* user_ops; //stores linked list of user operators
 
+static int isWindow = 0;
+
 static int num_errors = 0;
 static int curr_line_num = 1;
 
@@ -973,9 +975,13 @@ void beginVarScope(void) {
 void endVarScope(void) {
     namespace_head = namespace_head->next;
     if (namespace_head->next_var_num % 2 == 0) {
-        printf("    lea %d(%%rbp),%%rsp\n", 8 * (namespace_head->next_var_num));
+        if (!isWindow) {
+            printf("    lea %d(%%rbp),%%rsp\n", 8 * (namespace_head->next_var_num));
+        }
     } else {
-        printf("    lea %d(%%rbp),%%rsp\n", 8 * (namespace_head->next_var_num + 1));
+        if (!isWindow) {
+            printf("    lea %d(%%rbp),%%rsp\n", 8 * (namespace_head->next_var_num + 1));
+        }
     }
 }
 
@@ -1656,6 +1662,7 @@ int statement(int perform) {
         consume();
         return 1;
     } else if (isWindowStart()) {
+        isWindow = 1;
         consume();
         if(!isInt()){
             error(GENERAL, "Expected window x size after declaring window start block\n");
@@ -1756,6 +1763,7 @@ int statement(int perform) {
                 statement(perform);
             }
         }
+        isWindow = 0;
         consume();
         return 1;   
     } else if (isIf()) {
